146dd6f67f3d49d3f5304ef8fa8e5fa8d63339e2
Adaji Mukhtar <alfadaji@gmail.com>
2022-06-19 17:32:20 +0100

    changed fetch api method to axios

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import { useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
+
+  if (isLoading) {
+    return(
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   return (
     <main>
       {isOpen && <Modal />}
